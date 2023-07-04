c6d7d96492763ba6d0b817ccf5b4aaeada46c0ae
EC2 Default User <ec2-user@ip-172-31-14-101.ap-northeast-1.compute.internal>
2023-07-04 06:10:24 +0000

    debugfinish

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  has_many :books
+  has_many :books, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
   
   validates :name, presence: true
@@ -16,7 +17,16 @@ class User < ApplicationRecord
  
   
   
-  def get_profile_image
-    (profile_image.attached?) ? profile_image : 'no_image.jpg'
-  end
+  # def get_profile_image
+  #   (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  # end
+  
+   def get_profile_image(width, height)
+     unless profile_image.attached?
+     file_path = Rails.root.join('app/assets/images/no_image.jpg')
+     profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+     end
+     profile_image.variant(resize_to_limit: [width, height]).processed
+   end
+         
 end
