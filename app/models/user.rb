24b6c16a07140d6d41a6fdaa095792b0e948b645
EC2 Default User <ec2-user@ip-172-31-14-101.ap-northeast-1.compute.internal>
2023-07-07 16:24:26 +0000

    search

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,21 +6,41 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+ 
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
+
+
   has_one_attached :profile_image
-  
+
   validates :name, presence: true
-  
+
   validates :introduction, length:  { maximum: 50 }
   validates :name, length: { in: 2..20 }, uniqueness: true
-  
-  
- 
-  
-  
+
+
+
+
+
   # def get_profile_image
   #   (profile_image.attached?) ? profile_image : 'no_image.jpg'
   # end
-  
+  def follow(user)
+    relationships.create(followed_id: user.id)
+  end
+
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
+
    def get_profile_image(width, height)
      unless profile_image.attached?
      file_path = Rails.root.join('app/assets/images/no_image.jpg')
@@ -28,5 +48,5 @@ class User < ApplicationRecord
      end
      profile_image.variant(resize_to_limit: [width, height]).processed
    end
-         
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
+
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
+  get "search" => "searches#search"
   
   
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
@@ -10,7 +14,15 @@ Rails.application.routes.draw do
     resources :post_comments, only: [:create, :destroy]
     
   end
-  resources :users, only: [:index,:show,:edit,:update]
+  
+  
+  
+  resources :users, only: [:index,:show,:edit,:update] do
+      resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  
+  end
   
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
